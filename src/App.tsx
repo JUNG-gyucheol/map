9ff20d50e16c1b56610b2df4c4d2f0a9b67a1463
Jung gyu cheol <kcpooh0202@naver.com>
2022-02-14 07:53:07 +0900

    Modify map

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,42 @@
-import { useEffect, useRef } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 function App() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const draw = (cameraOffset: { x: number; y: number }) => {
+    const dpr = window.devicePixelRatio;
+    const canvas = canvasRef.current;
+
+    if (!canvas) return;
+
+    const ctx = canvas.getContext('2d');
+
+    canvas.width = document.body.clientWidth * dpr;
+    canvas.height = document.body.clientHeight * dpr;
+
+    if (!ctx) return;
+
+    const centerX = document.body.clientWidth / 2;
+    const centerY = document.body.clientHeight / 2;
+    ctx.scale(dpr / 2, dpr / 2);
+    ctx.clearRect(0, 0, document.body.clientWidth, document.body.clientHeight);
+    // ctx.translate(centerX, centerY);
+    ctx.translate(-centerX / 2 + cameraOffset.x * 1.5 - 50, -centerY / 2 + cameraOffset.y * 1.5 - 50);
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, 1000, 1000);
+
+    ctx.strokeStyle = 'white';
+    ctx.fillStyle = 'lightgreen';
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 10; j++) {
+        ctx.rect(100 * j, 100 * i, 100, 100);
+      }
+    }
+    ctx.fill();
+    ctx.stroke();
+
+    console.log('move1');
+  };
 
   useEffect(() => {
     const dpr = window.devicePixelRatio;
@@ -11,37 +46,67 @@ function App() {
     let MAX_ZOOM = 5;
     let MIN_ZOOM = 0.1;
 
-    const centerX = document.body.clientWidth / 2;
-    const centerY = document.body.clientHeight / 2;
-
+    let isDragging = false;
+    let dragStart = { x: 0, y: 0 };
+    let cameraOffset = {
+      x: document.body.clientWidth / 2,
+      y: document.body.clientHeight / 2,
+    };
     if (!canvas) return;
-    canvas.addEventListener('mousedown', e => {
-      console.log('mouse down');
+
+    // const ctx = canvas.getContext('2d');
+
+    // canvas.width = document.body.clientWidth * dpr;
+    // canvas.height = document.body.clientHeight * dpr;
+
+    // if (!ctx) return;
+
+    const pointDown = (e: MouseEvent) => {
+      isDragging = true;
+      // setDragStart({
+      //   x: e.clientX / cameraZoom - cameraOffset.x,
+      //   y: e.clientY / cameraZoom - cameraOffset.y,
+      // });
+      // const {x,y} = pointDownn(e,isDragging,cameraZoom);
+      dragStart.x = e.clientX / cameraZoom - cameraOffset.x;
+      dragStart.y = e.clientY / cameraZoom - cameraOffset.y;
+    };
+
+    const pointUp = () => {
+      isDragging = false;
+    };
+
+    const pointMove = (e: MouseEvent) => {
+      if (isDragging) {
+        // setCameraOffset({
+        //   x: e.clientX / cameraZoom - dragStart.x,
+        //   y: e.clientY / cameraZoom - dragStart.y,
+        // });
+        cameraOffset.x = e.clientX / cameraZoom - dragStart.x;
+        cameraOffset.y = e.clientY / cameraZoom - dragStart.y;
+        draw(cameraOffset);
+      }
+    };
+
+    draw(cameraOffset);
+    canvas.addEventListener('mousedown', pointDown);
+    canvas.addEventListener('mouseup', pointUp);
+    canvas.addEventListener('mousemove', pointMove);
+
+    canvas.addEventListener('click', e => {
       console.log(e.clientX, e.clientY);
     });
-    canvas.addEventListener('mouseup', () => {
-      console.log('mouse up');
-    });
-    canvas.addEventListener('mousemove', () => {
-      console.log('mouse move');
-    });
 
-    const ctx = canvas.getContext('2d');
-
-    if (!ctx) return;
-
-    canvas.width = document.body.clientWidth * dpr;
-    canvas.height = document.body.clientHeight * dpr;
-
-    ctx.scale(cameraZoom, cameraZoom);
-
-    ctx.beginPath();
-    ctx.moveTo(centerX, centerY);
-    ctx.rect(centerX - 50, centerY - 50, 100, 100);
-    // ctx.closePath();
-    ctx.stroke();
-    console.log('asasa');
+    return () => {
+      canvas.removeEventListener('mousedown', pointDown);
+      canvas.removeEventListener('mouseup', pointUp);
+      canvas.removeEventListener('mousemove', pointMove);
+    };
   }, []);
+
+  // useEffect(() => {
+  //   draw();
+  // }, [cameraOffset, draw]);
 
   return (
     <div
